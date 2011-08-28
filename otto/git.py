bfbbd5d057db34d6199f1b337152ec797cc62ff7
Vince Veselosky <vince@control-escape.com>
2011-08-28 10:36:09 -0400

    Port to the new config method

--- otto/git.py
+++ otto/git.py
@@ -1,23 +1,18 @@
+# encoding: UTF-8
 """
 Utilities and fabric tasks for working with git.
 
 """
 
-from fabric.api import abort, cd, env, local, puts, run, sudo, task
+from fabric.api import abort, cd, env, local, puts, require, run, sudo, task
 import fabric.contrib.files as remotefile
 import os
 import os.path
 
-default_settings = {"remote_repo_path": ''}
-settings = env.otto.get("git", {})
-for k, v in default_settings.iteritems():
-    settings.setdefault(k, v)
-env.otto['git'] = settings
-
 @task
 def create_origin(repo=None):
-    """Create an upstream origin repository from the current local one."""
-
+    """Create upstream origin repo from current local one."""
+    require('otto.git.remote_repo_path')
     # Ensure we're actually in a git working dir
     project_dir = os.getcwd()
     while not os.path.exists(os.path.join(project_dir, '.git')):
@@ -36,9 +31,9 @@ def create_origin(repo=None):
     repo = repo or os.path.basename(project_dir)
     if not repo.endswith('.git'):
         repo = repo+'.git'
-    remote_path = env.otto['git']['remote_repo_path']
+    remote_path = env['otto.git.remote_repo_path']
     if not remote_path.startswith('/'):
-        abort('"remote_repo_path" must point to an absolute path (beginning with "/")')
+        abort('"remote_repo_path" must be an absolute path (beginning with "/")')
 
     if remote_path.endswith('/'):
         remote_path = remote_path[:-1]
@@ -75,7 +70,6 @@ def clone_or_update(target,repo,branch='master',use_sudo=False):
         with cd(target):
             action('git checkout %s' % branch)
 
-@task
 def check_settings():
     """Just a test to see if otto has settings"""
     if isinstance(env.otto, dict):
