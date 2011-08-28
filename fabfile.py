067d412ee6c5531b49561cd2b5e2fc69595a25ac
Vince Veselosky <vince@control-escape.com>
2011-08-28 10:46:11 -0400

    Add web deployment task library.
    Also cleaned up the top-level fab file, almost everything has moved to
    a library now.

--- fabfile.py
+++ fabfile.py
@@ -1,67 +1,20 @@
 """Fabric tasks to automate my webserver infrastructure."""
 
-from fabric.api import cd, env, run, settings, sudo
+from fabric.api import env, sudo
 import fabric.contrib.files as remotefile
 import otto.git as git
+import otto.web as web
+import otto.ubuntu.lucidlynx as server
 
 env['ottohome'] = '/usr/local/share/otto-webber'
-env['verbose'] = False
-env.otto['git'] = {"remote_repo_path":"/home/vince/Repo/"}
+env['verbose'] = True
+env['otto.git.remote_repo_path'] = "/home/vince/Repo/"
 
 # TODO boto tasks to bring EC2 servers up and down
 # TODO Configure an FTP Server.
 # TODO Implement disk quotas for users
 
-# 1. Assume Ubuntu 10.10 server base
-PACKAGES = {
-    'ANYSERVER':
-        ['build-essential', 'curl', 'git', 'python-dev', 'python-pip', 'python-virtualenv',
-         'screen', 'tree', 'vim-nox'
-        ],
-    'WEBSERVER':
-        ['apache2-mpm-worker', 'apache2.2-common', 'apache2-suexec-custom',
-         'libapache2-mod-fcgid', 'libapache2-mod-wsgi',
-         'mysql-client-5.1',
-         'python-mysqldb',
-         'libjpeg-progs', 'optipng', # image optimization tools for html5boilerplate build
-        ],
-    'PHP':
-        ['php5-cli', 'php5-cgi', 'php5-adodb', 'php5-gd', 'php5-imagick',
-         'php5-mysql', 'php5-suhosin', 'php-pear',
-        ],
-    'DATABASE':
-        ['mysql-server-5.1', 'mysql-client-5.1'],
-}
-PACKAGES['FULLSTACK'] = PACKAGES['WEBSERVER'] + PACKAGES['DATABASE']
-
 # TASKS
-
-def setup_server(servertype="FULLSTACK", mysqlpassword=None):
-    """Install and configure requisite system packages on a bare server."""
-    sudo('apt-get -qq update')
-    sudo('apt-get -qy upgrade')
-    # Setting frontend to noninteractive prevents prompting for mysql server
-    # root password. This means mysql server root password will be empty.
-    # TODO Use debconf to set a mysql server root password.
-    SERVER_PACKAGES = PACKAGES['ANYSERVER'] + PACKAGES[servertype.upper()]
-    sudo('DEBIAN_FRONTEND=noninteractive apt-get -qy install '
-        + ' '.join(SERVER_PACKAGES)
-        )
-    # But until we can configure passwd beforehand, we'll do it after.
-    if 'mysql-server-5.1' in SERVER_PACKAGES:
-        try:
-            mysqlpassword = mysqlpassword or env.mysql.password
-        except AttributeError:
-            pass
-        if mysqlpassword:
-            # Note: If the password has already been set, this will fail, hence
-            # the "warn only".
-            with settings(warn_only=True):
-                run('mysqladmin -u root password "%s"' % mysqlpassword)
-
-    # install some python tools at the global level. Because I said so.
-    sudo('pip install fabric sphinx')
-
 
 def install_otto(force_update=False):
     """Install (or upgrade) the otto-webber code on a server.
@@ -87,55 +40,4 @@ def new_user(username):
     """Create a new user account."""
     sudo('/usr/sbin/adduser --conf %s/server/adduser.conf %s' % (env['ottohome'], username))
 
-
-def setmeup():
-    """Setup otto-webber for an existing user.
-
-    For now, can only setup the user who is executing the command, but that's a bug.
-    """
-    run('mdkdir -p ~/otto-webber')
-
-
-# TODO Task to set up new Apache VirtualHost for a named user.
-def new_site(domain):
-    """Create a new web site for the named user."""
-    pass
-
-
-def remote_deploy(name, repo, branch='master'):
-    """Deploy a site from a git repo."""
-    # Ensure latest otto code is on server.
-    install_otto()
-
-    # ensure home dir is prepared for otto
-    setmeup()
-    install_path = '~/otto-webber/' + name
-
-    # TODO Disable crontab on server to prevent run with half-deployed code,
-    # and to ensure if crontab is removed from new deployment the old one will
-    # no longer be running.
-
-    # check out the repo
-    git.clone_or_update(install_path, repo, branch)
-
-    # Run the local_deploy task for the fresh repo
-    with cd(env['ottohome']):
-        run('fab local_deploy ' + install_path)
-
-
-def local_deploy(dir):
-    """Deploy a site on the local system.
-
-    The remote_deploy task calls local_deploy on the server, to run python
-    code on the server itself. The `dir` argument is where the repo has been
-    checked out.
-    """
-    pass
-
-
-# TODO Task to install Wordpress for a named user.
-
-def test_remotes():
-    """Test how fab works"""
-    run('ls ~/bin')
 
---
+++ otto/web.py
@@ -0,0 +1,140 @@
+# encoding: UTF-8
+"""Web deployment tasks for fabric.
+
+These tasks are meant to be run as the "deployer". The deployer is a remote
+user who:
+
+* has certain sudo privileges (FIXME document which privileges),
+* has write permission to deployment directory (env['otto.web.site_root']),
+* is *not* the same user that your web server runs as (i.e. not www-data).
+
+Set the following keys in your `env` to configure otto.web:
+
+`otto.web.build_dir`
+    *Required.* The directory where your build task assembles the files to be
+    uploaded.
+
+`otto.web.site`
+    *Required.* The domain name of the site Otto will be manipulating. This
+    name is used to construct several paths on the remote server.
+
+.. TODO:
+
+    Document optional keys for constructing `site_dir`.
+"""
+
+from datetime import datetime
+from fabric.api import  abort, cd, env, local, require, run, sudo, task
+import fabric.contrib.files as remotefile
+from fabric.contrib.project import upload_project
+import os.path
+
+DEFAULT_CONFIG = {
+    'otto.web.prefix': '/usr/local',
+    'otto.web.site_root': 'share/apache2/sites', # relative to prefix
+    'otto.web.site_dir': '%(otto.web.prefix)s/%(otto.web.site_root)s/%(otto.web.site)s',
+    }
+for k, v in DEFAULT_CONFIG.iteritems():
+    env.setdefault(k, v)
+
+def _site_dir():
+    """calculate the site directory"""
+    require('otto.web.site', 'otto.web.site_root', 'otto.web.site_dir')
+    site_dir = env['otto.web.site_dir'] % env
+    if not site_dir.startswith('/'):
+        abort('Site directory is not absolute: "%s"' % site_dir)
+    return site_dir
+
+@task
+def stage():
+    """Upload site content to web server."""
+    require('otto.web.build_dir')
+    site_dir = _site_dir()
+    deploy_ts = env['otto.web.deploy_ts'] = datetime.utcnow().isoformat()
+
+    # Rename the local build dir to match to deploy_ts
+    # Must remove trailing slash or os.path.dirname(x) returns x!
+    build_dir = env['otto.web.build_dir']
+    if build_dir.endswith('/'):
+        build_dir = build_dir[:-1]
+    dirname, basename = os.path.split(build_dir)
+    localname = os.path.join(dirname, deploy_ts)
+    local('mv %s %s' % (build_dir, localname))
+
+    # Upload the content to the server.
+    upload_project(localname, site_dir)
+
+    # Update the "previous" and "current" links
+    with cd(site_dir):
+        run("ln -sfn %s staged" % deploy_ts)
+
+@task
+def deploy():
+    """Make your staged site content "live"."""
+    # TODO Support deployment of named builds by passing name arg.
+    site_dir = _site_dir()
+    if not remotefile.exists(site_dir + '/staged'):
+        abort("No staged build to deploy! Use the stage task first.")
+    # Update the "previous" and "current" links
+    with cd(site_dir):
+        run("""
+        if [ -L current ]; then
+            ln -sfn `readlink current` previous
+        fi
+        """)
+        run("ln -sfn `readlink staged` current && rm staged")
+
+@task
+def rollback():
+    """Undo deployment, roll back to the previous deploy."""
+    site_dir = _site_dir()
+    if not remotefile.exists(site_dir + '/previous'):
+        abort("No previous deployment to roll back to!")
+    with cd(site_dir):
+        run("""
+            ln -sfn `readlink current` rolledback
+            ln -sfn `readlink previous` current
+        """)
+
+@task
+def clean_server():
+    """Remove old unused deployments from server."""
+    # TODO Smarter code so that any build that has a symlink is preserved.
+    site_dir = _site_dir()
+    with cd(site_dir):
+        run("""
+        current=`readlink current`
+        previous=`readlink previous`
+        staged=`readlink staged`
+        for dir in `ls`; do
+            [ $dir != "$current" ] && [ $dir != "$staged" ] && [ $dir != "$previous" ] && [ $dir != "current" ] && [ $dir != "staged" ] && [ $dir != "previous" ] && rm -rf $dir
+        done
+        echo "Cleaned old deployments."
+        """)
+
+@task
+def reload_apache():
+    """Force apache to reload its configuration."""
+    sudo('/etc/init.d/apache2 reload')
+
+@task
+def enable_site():
+    """Install apache support for the virtual host."""
+    require('otto.web.site')
+    site_dir = _site_dir()
+    site = env['otto.web.site']
+
+    vhost_path = "%s/current/etc/apache2/vhost.d/%s" % (site_dir, site)
+    dest = '/etc/apache2/sites-available/'
+    sudo("cp %s %s" % (vhost_path, dest))
+    sudo("a2ensite %s" % site)
+    reload_apache()
+
+@task
+def disable_site():
+    """Remove apache support for the virtual host."""
+    sudo("a2dissite %s" % env['otto.web.site'])
+    reload_apache()
+    # ¿Physically remove the conf from sites-available?
+
+
