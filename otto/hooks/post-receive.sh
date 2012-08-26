f1c7855d36b2f91da89f329ad426e9bbbbc18c02
Vince Veselosky <vince@control-escape.com>
2012-08-26 11:34:22 -0400

    speculation on what hook should do

--- otto/hooks/post-receive.sh
+++ otto/hooks/post-receive.sh
@@ -6,15 +6,27 @@ set -e # exit immediately on error
 # Git hooks drop you in the .git dir of your repo, setting $GIT_DIR=.
 REPO=$(dirname `pwd`)
 BASENAME=`basename $REPO`
-cd ../../../workspace/$BASENAME
+cd ../../../
+OTTO_HOME=`pwd`
+cd workspace/$BASENAME
 
 # Must change the GIT_DIR or these commands will operate on the wrong repo.
 # Do it in a sub-shell just in case.
 (
 GIT_DIR=.git
-# Determine the latest otto-* tag.
+
+# Get latest code
 git fetch
+
+# Determine the latest otto-* tag.
 TAG=git tag -l otto-* | sort | awk 'END {print}'
+
+# Install checkout latest tag, trashing local changes
 git reset --hard $TAG
-fab build web.rsync web.install_etc
+
+# Calculate the right virtualenv and activate
+ENVHASH=md5sum requirements.txt | awk {print $1}
+source $OTTO_HOME/virtualenvs/$ENVHASH/bin/activate
+
+fab build web.golive
 )
